4a7f0e29cc3719c07ebd826f4c5d10e5657dca11
CamillaKurup <4342295@RTS-365.dk>
2017-08-18 09:45:50 +0200

    aflevering

---
+++ opg1.js
@@ -0,0 +1,18 @@
+//1.1 og 1.2//
+var cars = 
+[
+    "Saab", 
+    "volvo", 
+    "BMW"
+];
+console.log (cars[1])
+
+//1.3//
+var cars = 
+[
+    "Saab", 
+    "volvo", 
+    "BMW"
+];
+cars[0] = "Opel";
+console.log (cars)
